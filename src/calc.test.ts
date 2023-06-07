f041e133812b5a7bcfd82f3df38f40ff72f584bd
Amina-Dirie <adirie522@gmail.com>
2023-06-07 07:49:30 -0700

    feat: finishing pairwords

--- src/calc.test.ts
+++ src/calc.test.ts
@@ -1,9 +1,28 @@
-import { add } from "../src/calc";
-describe("test add function", () => {
-  it("should return 15 for add(10,5)", () => {
-    expect(add(10, 5)).toBe(15);
+// import { add } from "../src/calc";
+// describe("test add function", () => {
+//   it("should return 15 for add(10,5)", () => {
+//     expect(add(10, 5)).toBe(15);
+//   });
+//   it("should return 5 for add(2,3)", () => {
+//     expect(add(2, 3)).toBe(5);
+//   });
+// });
+import { pairWords } from "../src/calc";
+describe("pairWords function", () => {
+  it("should split 'abc' into ['ab', 'c_']", () => {
+    expect(pairWords("abc")).toEqual(["ab", "c_"]);
   });
-  it("should return 5 for add(2,3)", () => {
-    expect(add(2, 3)).toBe(5);
+
+  it("should split 'abcdef' into ['ab', 'cd', 'ef']", () => {
+    expect(pairWords("abcdef")).toEqual(["ab", "cd", "ef"]);
   });
+
+  it("should split 'a' into ['a_']", () => {
+    expect(pairWords("a")).toEqual(["a_"]);
+  });
+  it("should split '' into []", () => {
+    expect(pairWords("")).toEqual([]);
+  });
+
+  
 });
--- src/calc.ts
+++ src/calc.ts
@@ -1,3 +1,21 @@
-export function add(x: number, y: number): number {
-  return x + y;
+// export function add(x: number, y: number): number {
+//   return x + y;
+// }
+
+export function pairWords(word: string): string[]{
+  let result = []
+  let splited = word.split('');
+ 
+  for (let i = 0; i <= splited.length; i+=2) {
+    if(i < splited.length/2+2){
+      if(splited[i] !== splited[splited.length-1]){
+        result.push(`${splited[i]}${splited[i+1]}`)
+      }else{ 
+        result.push(`${splited[i]}_`)
+      }
+    }
+    
+  }
+
+  return splited.length >0 ? result : [];
 }
